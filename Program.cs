1e6084ccc89f3f7dc034467eca68693c7855e95a
LuisRoca11 <lroca1102@gmail.com>
2025-09-16 15:08:08 -0300

    se calculan multiplos de 11

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. 
+// (No se ingresan valores por teclado)
+
+using System;
+
+namespace mutiplos_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplo = 11, suma = 0, x = 1;
+
+            while (x <= 25)
+            {
+                Console.WriteLine(multiplo);
+                multiplo = multiplo + 11;
+                x = x + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
